804fb1b799c5289cc12774da44a9b4e9e894d198
Jonathan Oesterberg <jonathan@hefringmarine.com>
2026-05-16 23:49:51 +0000

    Major redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@
 	};
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
+		$(document).ready(function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
@@ -122,21 +122,7 @@
 		$tiles.each(function() {
 
 			var $this = $(this),
-				$image = $this.find('.image'), $img = $image.find('img'),
-				$link = $this.find('.link'),
-				x;
-
-			// Image.
-
-				// Set image.
-					$this.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Set position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide original.
-					$image.hide();
+				$link = $this.find('.link');
 
 			// Link.
 				if ($link.length > 0) {
